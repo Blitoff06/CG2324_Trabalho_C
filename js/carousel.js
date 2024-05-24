c6e5456743e90b67f36e80731d4e64a707a7db29
Tiago Brito <tiago.m.brito@tecnico.ulisboa.pt>
2024-05-24 14:27:01 +0100

    fix textureURL

--- js/carousel.js
+++ js/carousel.js
@@ -550,7 +550,7 @@ function createFloor() {
 }
 
 function createSkydome() {
-  const textureURL = "/docs/assets/images/an_optical_poem.PNG";
+  const textureURL = "./docs/assets/images/an_optical_poem.PNG";
 
   const skyDome = skydomeGeometryConstructor(skydome_radius, textureURL);
   scene.add(skyDome);
